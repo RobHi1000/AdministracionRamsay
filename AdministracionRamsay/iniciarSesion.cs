dda0050ce00f6c1e8ff473001bc53d1f393668af
Roberto Becerra <roberto.becerra@uabc.edu.mx>
2019-06-11 02:16:47 -0700

    Se agrego atajo en el teclado al boton enter en el campo de password

--- AdministracionRamsay/iniciarSesion.cs
+++ AdministracionRamsay/iniciarSesion.cs
@@ -10,11 +10,50 @@ using System.Windows.Forms;
 
 namespace AdministracionRamsay
 {
-    public partial class iniciarSesion : Form
+    public partial class IniciarSesion : Form
     {
-        public iniciarSesion()
+        public IniciarSesion()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void iniciarSesion_Load(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonIngresar_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Principal Ramsay = new Principal();
+            Ramsay.ShowDialog();
+            this.Close();
+        }
+
+        private void textBoxContraseña_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                buttonIngresar_Click(sender, e);
         }
     }
 }
